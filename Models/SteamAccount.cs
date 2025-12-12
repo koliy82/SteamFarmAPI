20233f4afc554e001944758d9f99f20fe7a9e1df
Koliy82 <sokol070804@mail.ru>
2025-12-12 03:56:10 +0300

    Update SteamAccount.cs

--- Models/SteamAccount.cs
+++ Models/SteamAccount.cs
@@ -10,15 +10,19 @@ namespace SteamAPI.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = string.Empty;
-
+        [BsonElement("telegram_id")]
         public long TelegramId { get; set; } = telegramId;
+        [BsonElement("username")]
         public string? Username { get; set; }
+        [BsonElement("password")]
         public string? Password { get; set; }
-
+        [BsonElement("refresh_token")]
         public string? RefreshToken { get; set; }
-
+        [BsonElement("game_ids")]
         public List<uint> GameIds { get; set; } = [570];
+        [BsonElement("is_farming")]
         public bool IsFarming { get; set; } = true;
+        [BsonElement("persona_state")]
         public EPersonaState PersonaState { get; set; } = EPersonaState.Online;
     }
 }
